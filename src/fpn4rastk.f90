1558c2cd31c41f35aaa1fc10541b7daa814e2434
alexlc <alex4444@list.ru>
2018-05-18 19:12:03 +0900

    Fix list and values of input variables

--- src/fpn4rastk.f90
+++ src/fpn4rastk.f90
@@ -7,9 +7,13 @@ module fpn4rastk
 
     type(frapcon_type) :: fpn
 
+    ! TEMPORARY VARIABLES
+    integer :: i
+    real(8) :: a, b, c
+
 contains
 
-    subroutine init(nr, na, x)
+    subroutine init(nr, na, x, radfuel, radgap, radclad, pitch, den, enrch, p2, tw, go, qmpy)
 
         integer :: na          ! number of nodes in the axial mesh
         integer :: nr          ! number of radial nodes in a pellet
@@ -20,12 +24,60 @@ contains
         integer :: ngasmod = 2 ! fission gas release model (1 = ANS5.4, 2 = Massih(Default), 3 = FRAPFGR, 4 = ANS5.4_2011)
 
         real(8) :: x(:)        ! Elevation in each qf, x array defining a power shape
+        real(8) :: radfuel
+        real(8) :: radgap
+        real(8) :: radclad
+        real(8) :: pitch
+        real(8) :: den
+        real(8) :: enrch
+        real(8) :: p2
+        real(8) :: qmpy
+        real(8) :: tw
+        real(8) :: go
 
         call fpn % init(na, ngasr, nr, nce, mechan, ngasmod)
 
-        fpn % x(:) = x(:)
+        fpn % cpl     = 6.3386d0 * cmtoin            ! Cold plenum length, in
+        fpn % crdt    = 0.d0 * cmtoin                ! Crud thickness, in
+        fpn % thkcld  = (radclad - radgap) * cmtoin  ! Thickness of cladding, in
+        fpn % thkgap  = (radgap - radfuel) * cmtoin  ! Thickness of gap, in
+        fpn % dco     = 2 * radclad * cmtoin         ! Outer cladding diameter, in
+        fpn % pitch   = pitch * cmtoin               ! Center to center rod distance, in
+        fpn % rc      = 0.d0 * cmtoin                ! Radius of the fuel pellet central annulus, in
+        fpn % fotmtl  = 2.d0                         ! Fuel oxygen-to-metal ratio
+        fpn % dishsd  = 0.064d0                      ! Dish shoulder width, in
+        fpn % den     = den * 10.40d0/10.96d0        ! As-fabricated apparent fuel density, %TD
+        fpn % dspg    = 0.3                          ! Spring diameter, in
+        fpn % fa      = 1.d0                         ! Peak-to-average power ratio
+        fpn % dspgw   = 0.0394                       ! Spring wire diameter, in
+        fpn % enrch   = enrch                        ! Fuel U-235 enrichment (atom per total heavy metal atoms)
+        fpn % fgpav   = 340.84                       ! Fill gas pressure, psi
+        fpn % hdish   = 0.0094d0                     ! Dish height, in
+        fpn % hplt    = 0.387d0                      ! Pellet height, in
+        fpn % icm     = 4                            ! Cladding type, 4: Zircaloy-4
+        fpn % imox    = 0                            ! Fuel type, 0: UO_2
+        fpn % idxgas  = 1                            ! Fill gas type (1 = He, 2 = Air, 3 = N2, 4 = FG, 5 = Ar, 6 = User-Specified)
+        fpn % iplant  =-2                            ! Plant type, -2: PWR, -3: BWR, -4: HBWR
+        fpn % totl    = x(size(x)) * cmtoft          ! Total length of active fuel, ft
+        fpn % roughc  = 1.97d-5                      ! Clad roughness, in
+        fpn % roughf  = 7.87d-5                      ! Fuel roughness, in
+        fpn % vs      = 10.d0                        ! Number of spring turns
+        fpn % rsntr   = 100.d0                       ! Expected resintering density increase, kg/m**3
+        fpn % nsp     = 0                            ! Specify which type of coolant conditions to use (0 = constant, 1 = time-dependent)
+        fpn % slim    = 0.05d0                       ! User supplied swelling limit (vol fraction) (Default = 0.05)
+        fpn % enrpu39 = 0.d0                         ! Fuel pellet Pu-239 content
+        fpn % enrpu40 = 0.d0                         ! Fuel pellet Pu-240 content
+        fpn % enrpu41 = 0.d0                         ! Fuel pellet Pu-241 content
+        fpn % enrpu42 = 0.d0                         ! Fuel pellet Pu-242 content
+        fpn % x(:)    = x(:) * cmtoft                ! Elevation in each qf, x array defining a power shape, ft
+        fpn % tw(1)   = tcf(tw)                      ! Coolant inlet temperature, F
+        fpn % p2(1)   = p2 * MPatoPSI                ! Coolant System Pressure, Psi
+        fpn % go(1)   = go * ksm2tolbhrft2           ! Coolant mass flux around fuel rod, lb/hr * ft^2
+        fpn % qf(:)   = 1.                           ! Ratio of linear power
+        fpn % qmpy    = 1.D-3 * qmpy / mtoft         ! The linear heat generation rate, kW/ft
 
-        call fpn % make()
+        call fpn % make() ! set default and check input variables
+        call fpn % stp0() ! make the very first time step
 
     end subroutine init
 
@@ -37,13 +89,60 @@ contains
 
     end subroutine next
 
-    subroutine set_linear_power_ratio(qf)
+    subroutine set(key, var)
 
-        real(8) :: qf(:) ! Ratio of linear power at x(n) elevation to axially average value for each M-th power shape
+        character(*) :: key
+        real(8)      :: var(:)
 
-        fpn % qf(:) = qf(:)
+        select case(key)
+        case("linear power")
+            fpn % qf(:) = var(:) / sum(var) * size(var)
+            fpn % qmpy  = 1.D-3 * sum(var) / mtoft
+        case("coolant temperature")
+            fpn % tcoolant(:) = (/( tcf(var(i)), i = 1, size(var) )/)
+        case("coolant pressure")
+            fpn % pcoolant(:) = var(:) * MPatoPSI
+        case("coolant mass flux")
+            fpn % go(1) = var(1) * ksm2tolbhrft2
+        case default
+            write(*,*) 'ERROR: Variable', key, ' has not been found'
+            stop
+        end select
 
-    end subroutine set_linear_power_ratio
+    end subroutine set
 
+    subroutine get(key, var)
+
+        character(*) :: key
+
+        real(8) :: var(:)
+
+        select case(key)
+        case('fuel average temperature')
+            !var = fpn % tmpfuel(:,:)
+            var(:) = 0
+        case('coolant average temperature')
+            var(:) = 0
+        case('gap conductance')
+            var(:) = 0
+        case('oxide thickness')
+            var(:) = 0
+        case('gap thickness')
+            var(:) = 0
+        case('gap pressure')
+            var(:) = 0
+        case('hoop strain')
+            var(:) = 0
+        case('hoop stress')
+            var(:) = 0
+        case default
+            write(*,*) 'ERROR: Variable ', key, ' has not been found'
+            stop
+        end select
+
+    end subroutine get
 
 end module fpn4rastk
+
+
+
--- src/test.f90
+++ src/test.f90
@@ -1,5 +1,5 @@
 program test
-    use fpn4rastk, only : init, next, set_linear_power_ratio
+    use fpn4rastk, only : init, next, get, set
 
     implicit none
 
@@ -7,6 +7,7 @@ program test
     character(len = 32) :: oname
 
     integer :: i_input_file = 42
+    integer :: i_output_file = 43
 
     integer(4) :: n_fuel_rad_in
     integer(4) :: na_r
@@ -15,7 +16,7 @@ program test
     integer(4) :: run_media
     integer(4) :: iz
     integer(4) :: i_bu_step
-    
+
     real(8) :: init_enrich
     real(8) :: pitch_in
     real(8) :: gap_rad
@@ -24,35 +25,49 @@ program test
     real(8) :: inlet_temp_in
     real(8) :: mass_flow_rate_in
     real(8) :: init_den
-    real(8) :: pin_power    
     real(8) :: h_bu_step
+    real(8) :: p2
+    real(8) :: tw
+    real(8) :: go(1)
+    real(8) :: qmpy
 
     integer(4), allocatable :: z_meshes(:)
 
     real(8), allocatable  :: pow_hist(:,:)
     real(8), allocatable  :: line_pow_hist(:,:)    
     real(8), allocatable  :: thickness_RASTK(:)
+    real(8), allocatable  :: thickness_FRPCN(:)
     real(8), allocatable  :: height_FRPCN(:)
     real(8), allocatable  :: height_RASTK(:)
-    real(8), allocatable  :: bong_rad(:)  
     real(8), allocatable  :: tmp_time(:)
     real(8), allocatable  :: pow_hist_loc(:,:)
     real(8), allocatable  :: power_dist_in(:)
-    real(8), allocatable  :: height_FPN(:)
     real(8), allocatable  :: ctf_coo_pres(:,:)
     real(8), allocatable  :: ctf_coo_temp(:,:)
     real(8), allocatable  :: ctf_clad_temp(:,:)
 
+    real(8), allocatable   ::  fue_avg_temp(:)
+    real(8), allocatable   ::  coo_avg_temp(:)
+    real(8), allocatable   ::  fue_dyn_hgap(:)
+    real(8), allocatable   ::  t_oxidelayer(:)
+    real(8), allocatable   ::  t_fuecladgap(:)
+    real(8), allocatable   ::  gap_pressure(:)
+    real(8), allocatable   ::  contact_pres(:)
+    real(8), allocatable   ::  hoop_strain (:)
+    real(8), allocatable   ::  hoop_stress (:)
+
     ! ARRAYS NEEDED FOR SPLINE INTERPOLATION
     real(8) :: ispline
-    real(8), allocatable  :: ix(:), iy(:), b(:), c(:), d(:)
+    real(8), allocatable  :: ix1(:), iy1(:), b1(:), c1(:), d1(:)
+    real(8), allocatable  :: ix2(:), iy2(:), b2(:), c2(:), d2(:)
 
+    ! ARRAYS FOR FRAPCON
+    real(8), allocatable  :: qf_FRPCN(:)
+    real(8), allocatable  :: tcool_FRPCN(:)
+    real(8), allocatable  :: pcool_FRPCN(:)
 
-    ! LINEAR POWER RATIO FOR FRAPCON
-    real(8), allocatable  :: qf(:)
-
-    ! ADDITIONAL VARIABLES
-    integer :: i, j, k
+    ! ITERATIONAL VARIABLES
+    integer :: i
 
     ! READING INPUT FILE
 
@@ -74,6 +89,7 @@ program test
 
     allocate(z_meshes(1:na_in))
     allocate(thickness_RASTK(1:na_r))
+    allocate(thickness_FRPCN(1:na_in))
 
     read(i_input_file,*) z_meshes(1:na_in)
     read(i_input_file,*) thickness_RASTK(1:na_r)
@@ -91,12 +107,6 @@ program test
     allocate(ctf_coo_pres(1:na_in,1:n_bu))
     allocate(ctf_coo_temp(1:na_in,1:n_bu))
     allocate(ctf_clad_temp(1:na_in,1:n_bu))
-    allocate(qf(1:na_in+1))
-    allocate(ix(1:na_r))
-    allocate(iy(1:na_r))
-    allocate(b(1:na_r))
-    allocate(c(1:na_r))
-    allocate(d(1:na_r))
 
     read(i_input_file,*) tmp_time(1:n_bu)  
 
@@ -111,18 +121,49 @@ program test
             read(i_input_file,*) (line_pow_hist(iz,:), iz = 1, na_r)
     end select
 
+    close(i_input_file)
+
+    allocate(qf_FRPCN(1:na_in+1))
+    allocate(tcool_FRPCN(1:na_in+1))
+    allocate(pcool_FRPCN(1:na_in+1))
+    allocate(ix1(1:na_r))
+    allocate(iy1(1:na_r))
+    allocate(b1(1:na_r))
+    allocate(c1(1:na_r))
+    allocate(d1(1:na_r))
+    allocate(ix2(1:na_in))
+    allocate(iy2(1:na_in))
+    allocate(b2(1:na_in))
+    allocate(c2(1:na_in))
+    allocate(d2(1:na_in))
+
+    allocate(fue_avg_temp(1:na_in))
+    allocate(coo_avg_temp(1:na_in))
+    allocate(fue_dyn_hgap(1:na_in))
+    allocate(t_oxidelayer(1:na_in))
+    allocate(t_fuecladgap(1:na_in))
+    allocate(gap_pressure(1:na_in))
+    allocate(hoop_strain (1:na_in))
+    allocate(hoop_stress (1:na_in))
+
     ! DATA PROCESSING
     ! WTF ???
     if(tmp_time(1) .lt. 0.5) tmp_time(1) = 0.5d0
 
     ! HEIGHTS FOR RASTK and FRAPCON, LIKE (0.5, 1.0, 1.5, 2.0, ...)
     height_RASTK(1)  = 0.
-    height_RASTK(2:) = (/ (sum(thickness_RASTK(1:i)) , i = 1, na_r)  /)
+    height_RASTK(2:) = (/( sum(thickness_RASTK(1:i)) , i = 1, na_r )/)
     height_FRPCN(1)  = 0.
-    height_FRPCN(2:) = (/ (height_RASTK(sum(z_meshes(1:i))), i = 1, na_in ) /)
+    height_FRPCN(2:) = (/( height_RASTK(sum(z_meshes(1:i))+1), i = 1, na_in )/)
+    thickness_FRPCN(:) = height_FRPCN(2:na_in+1) - height_FRPCN(1:na_in)
 
-    ! FRAPCON INITIALIZATION 
-    call init(n_fuel_rad_in, na_in, height_FRPCN)
+    ! FRAPCON INITIALIZATION
+    p2 = ctf_coo_pres(1,1)
+    tw = ctf_coo_temp(1,1)
+    go(1) = mass_flow_rate_in
+    qmpy = sum(line_pow_hist(:,1))
+    call init(n_fuel_rad_in, na_in, height_FRPCN, fuel_rad, gap_rad, &
+    clad_rad, pitch_in, init_den, init_enrich, p2, tw, go(1), qmpy)
 
     ! FRAPCON RUN
 
@@ -131,18 +172,48 @@ program test
     do i_bu_step = 1, n_bu
 
         ! SPLINE INTERPOLATION OF THE RASP-K LINEAR POWER RATIO
-        ix(:) = height_RASTK(1:na_r) +  0.5 * thickness_RASTK(:)
-        iy(:) = line_pow_hist(:,i_bu_step)
-        call spline(ix, iy, b, c, d, na_r)
-        qf(:) = (/( ispline(height_FRPCN(i), ix, iy, b, c, d, na_r), i = 1, na_in+1 )/)
+        ix1(:) = height_RASTK(1:na_r) +  0.5 * thickness_RASTK(:)
+        iy1(:) = line_pow_hist(:,i_bu_step)
+        call spline(ix1, iy1, b1, c1, d1, na_r)
+        qf_FRPCN(:) = (/( ispline(height_FRPCN(i), ix1, iy1, b1, c1, d1, na_r), i = 1, na_in+1 )/)
+
+        ! SPLINE INTERPOLATION OF THE RASP-K COOLANT TEMPERATURE
+        ix2(:) = height_FRPCN(1:na_in) +  0.5 * thickness_FRPCN(:)
+        iy2(:) = ctf_coo_temp(:,i_bu_step)
+        call spline(ix2, iy2, b2, c2, d2, na_in)
+        tcool_FRPCN(:) = (/( ispline(height_FRPCN(i), ix2, iy2, b2, c2, d2, na_in), i = 1, na_in+1 )/)
+
+        ! SPLINE INTERPOLATION OF THE RASP-K COOLANT PRESSURE
+        ix2(:) = height_FRPCN(1:na_in) +  0.5 * thickness_FRPCN(:)
+        iy2(:) = ctf_coo_pres(:,i_bu_step)
+        call spline(ix2, iy2, b2, c2, d2, na_in)
+        pcool_FRPCN(:) = (/( ispline(height_FRPCN(i), ix2, iy2, b2, c2, d2, na_in), i = 1, na_in+1 )/)
 
         ! SETUP THE UPDATED VARIABLES
-        call set_linear_power_ratio(qf)
+        call set("linear power", qf_FRPCN)
+        call set("coolant temperature", tcool_FRPCN)
+        call set("coolant pressure", pcool_FRPCN)
+        call set("coolant mass flux", go)
         call next(h_bu_step)
 
     enddo
 
-    close(i_input_file)
+    call get('fuel average temperature', fue_avg_temp)
+    call get('coolant average temperature', coo_avg_temp)
+    call get('gap conductance', fue_dyn_hgap)
+    call get('oxide thickness', t_oxidelayer)
+    call get('gap thickness', t_fuecladgap)
+    call get('gap pressure', gap_pressure)
+    call get('hoop strain', hoop_strain)
+    call get('hoop stress', hoop_stress)
+
+    open(i_output_file, file=oname, status='unknown')
+
+    do i = 1, na_in
+        write(i_output_file,'(100es13.5)') fue_avg_temp(i), coo_avg_temp(i), &
+        fue_dyn_hgap(i), t_oxidelayer(i) , t_fuecladgap(i), gap_pressure(i), &
+        hoop_strain(i), hoop_stress(i)
+    enddo
 
     write(*,*) 'done!'
 
