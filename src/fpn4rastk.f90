7191fd767cf2ed3968a78d74d50ddb93e52696f5
alexlc <alex4444@list.ru>
2018-05-28 12:38:06 +0900

    Have changed the name fpn to driver

--- src/fpn4rastk.f90
+++ src/fpn4rastk.f90
@@ -1,11 +1,11 @@
 module fpn4rastk
 
     use conversions
-    use frapcon41, only : frapcon_type
+    use frapcon41, only : driver_type
 
     implicit none
 
-    type(frapcon_type) :: fpn
+    type(driver_type) :: driver
 
     ! TEMPORARY VARIABLES
     integer :: i, j, n, m
@@ -40,54 +40,54 @@ contains
         n  = n_
         m  = m_
 
-        call fpn % init(n, ngasr, m+1, nce, mechan, ngasmod)
+        call driver % init(n, ngasr, m+1, nce, mechan, ngasmod)
 
-        fpn % cpl     = 6.3386d0                     ! Cold plenum length, in
-        fpn % crdt    = 0.d0 * cmtoin                ! Crud thickness, in
-        fpn % thkcld  = (radclad - radgap) * cmtoin  ! Thickness of cladding, in
-        fpn % thkgap  = (radgap - radfuel) * cmtoin  ! Thickness of gap, in
-        fpn % dco     = 2 * radclad * cmtoin         ! Outer cladding diameter, in
-        fpn % pitch   = pitch * cmtoin               ! Center to center rod distance, in
-        fpn % rc      = 0.d0 * cmtoin                ! Radius of the fuel pellet central annulus, in
-        fpn % fotmtl  = 2.d0                         ! Fuel oxygen-to-metal ratio
-        fpn % dishsd  = 0.064d0                      ! Dish shoulder width, in
-        fpn % den     = den * 10.40d0/10.96d0        ! As-fabricated apparent fuel density, %TD
-        fpn % dspg    = 0.3                          ! Spring diameter, in
-        fpn % fa      = 1.d0                         ! Peak-to-average power ratio
-        fpn % dspgw   = 0.0394                       ! Spring wire diameter, in
-        fpn % enrch   = enrch                        ! Fuel U-235 enrichment (atom per total heavy metal atoms)
-        fpn % fgpav   = 340.84                       ! Fill gas pressure, psi
-        fpn % hdish   = 0.0094d0                     ! Dish height, in
-        fpn % hplt    = 0.387d0                      ! Pellet height, in
-        fpn % icm     = 4                            ! Cladding type, 4: Zircaloy-4
-        fpn % idxgas  = 1                            ! Fill gas type (1 = He, 2 = Air, 3 = N2, 4 = FG, 5 = Ar, 6 = User-Specified)
-        fpn % iplant  =-2                            ! Plant type, -2: PWR, -3: BWR, -4: HBWR
-        fpn % imox    = 0                            ! Fuel type, 0: UO_2
-        fpn % totl    = sum(dx) * cmtoft             ! Total length of active fuel, ft
-        fpn % roughc  = 1.97d-5                      ! Clad roughness, in
-        fpn % roughf  = 7.87d-5                      ! Fuel roughness, in
-        fpn % vs      = 30.d0                        ! Number of spring turns
-        fpn % rsntr   = 100.d0                       ! Expected resintering density increase, kg/m**3
-        fpn % nsp     = 0                            ! Specify which type of coolant conditions to use (0 = constant, 1 = time-dependent)
-        fpn % slim    = 0.05d0                       ! User supplied swelling limit (vol fraction) (Default = 0.05)
-        fpn % enrpu39 = 0.d0                         ! Fuel pellet Pu-239 content
-        fpn % enrpu40 = 0.d0                         ! Fuel pellet Pu-240 content
-        fpn % enrpu41 = 0.d0                         ! Fuel pellet Pu-241 content
-        fpn % enrpu42 = 0.d0                         ! Fuel pellet Pu-242 content
-        fpn % tw(1)   = 600.                         ! Coolant inlet temperature, F
-        fpn % p2(1)   = 2000.                        ! Coolant System Pressure, Psi
-        fpn % go(1)   = 2.D+6                        ! Coolant mass flux around fuel rod, lb/hr * ft^2
-        fpn % qf(:)   = 1.                           ! Ratio of linear power
-        fpn % qmpy    = 6.                           ! The linear heat generation rate, kW/ft (after make it turns to rod average heat flux, BTU/(hr*ft^2) )
-        fpn % gadoln(:) = 0.d0                       ! Weight fraction of gadolinia in the fuel
+        driver % cpl     = 6.3386d0                     ! Cold plenum length, in
+        driver % crdt    = 0.d0 * cmtoin                ! Crud thickness, in
+        driver % thkcld  = (radclad - radgap) * cmtoin  ! Thickness of cladding, in
+        driver % thkgap  = (radgap - radfuel) * cmtoin  ! Thickness of gap, in
+        driver % dco     = 2 * radclad * cmtoin         ! Outer cladding diameter, in
+        driver % pitch   = pitch * cmtoin               ! Center to center rod distance, in
+        driver % rc      = 0.d0 * cmtoin                ! Radius of the fuel pellet central annulus, in
+        driver % fotmtl  = 2.d0                         ! Fuel oxygen-to-metal ratio
+        driver % dishsd  = 0.064d0                      ! Dish shoulder width, in
+        driver % den     = den * 10.40d0/10.96d0        ! As-fabricated apparent fuel density, %TD
+        driver % dspg    = 0.3                          ! Spring diameter, in
+        driver % fa      = 1.d0                         ! Peak-to-average power ratio
+        driver % dspgw   = 0.0394                       ! Spring wire diameter, in
+        driver % enrch   = enrch                        ! Fuel U-235 enrichment (atom per total heavy metal atoms)
+        driver % fgpav   = 340.84                       ! Fill gas pressure, psi
+        driver % hdish   = 0.0094d0                     ! Dish height, in
+        driver % hplt    = 0.387d0                      ! Pellet height, in
+        driver % icm     = 4                            ! Cladding type, 4: Zircaloy-4
+        driver % idxgas  = 1                            ! Fill gas type (1 = He, 2 = Air, 3 = N2, 4 = FG, 5 = Ar, 6 = User-Specified)
+        driver % iplant  =-2                            ! Plant type, -2: PWR, -3: BWR, -4: HBWR
+        driver % imox    = 0                            ! Fuel type, 0: UO_2
+        driver % totl    = sum(dx) * cmtoft             ! Total length of active fuel, ft
+        driver % roughc  = 1.97d-5                      ! Clad roughness, in
+        driver % roughf  = 7.87d-5                      ! Fuel roughness, in
+        driver % vs      = 30.d0                        ! Number of spring turns
+        driver % rsntr   = 100.d0                       ! Expected resintering density increase, kg/m**3
+        driver % nsp     = 0                            ! Specify which type of coolant conditions to use (0 = constant, 1 = time-dependent)
+        driver % slim    = 0.05d0                       ! User supplied swelling limit (vol fraction) (Default = 0.05)
+        driver % enrpu39 = 0.d0                         ! Fuel pellet Pu-239 content
+        driver % enrpu40 = 0.d0                         ! Fuel pellet Pu-240 content
+        driver % enrpu41 = 0.d0                         ! Fuel pellet Pu-241 content
+        driver % enrpu42 = 0.d0                         ! Fuel pellet Pu-242 content
+        driver % tw(1)   = 600.                         ! Coolant inlet temperature, F
+        driver % p2(1)   = 2000.                        ! Coolant System Pressure, Psi
+        driver % go(1)   = 2.D+6                        ! Coolant mass flux around fuel rod, lb/hr * ft^2
+        driver % qf(:)   = 1.                           ! Ratio of linear power
+        driver % qmpy    = 6.                           ! The linear heat generation rate, kW/ft (after make it turns to rod average heat flux, BTU/(hr*ft^2) )
+        driver % gadoln(:) = 0.d0                       ! Weight fraction of gadolinia in the fuel
 
         ! Elevation in each qf, x array defining a power shape, ft
-        fpn % x(1)     = 0.d0
-        fpn % x(2:n+1) = (/( sum(dx(:i)), i = 1, n )/) * cmtoft
-        fpn % deltaz(1:n)= dx(:) * cmtoft
-        fpn % deltaz(n+1)= fpn % cpl
+        driver % x(1)     = 0.d0
+        driver % x(2:n+1) = (/( sum(dx(:i)), i = 1, n )/) * cmtoft
+        driver % deltaz(1:n)= dx(:) * cmtoft
+        driver % deltaz(n+1)= driver % cpl
 
-        call fpn % make() ! set default and check input variables
+        call driver % make() ! set default and check input variables
 
         ! ALLOCATION OF THE TEMPORARY ARRAYS
         allocate(weight(m))
@@ -100,7 +100,7 @@ contains
 
     subroutine init()
 
-        call fpn % stp0()  ! make the very first time step
+        call driver % stp0()  ! make the very first time step
 
     end subroutine init
 
@@ -108,7 +108,7 @@ contains
 
        real(8) :: dt
 
-       call fpn % next(dt)
+       call driver % next(dt)
 
     end subroutine next
 
@@ -118,25 +118,25 @@ contains
         integer      :: it
         real(8)      :: var(:)
 
-        it = fpn % it
+        it = driver % it
 
         select case(key)
         case("linear power, W/cm")
-            call linterp(var, fpn % deltaz(1:n), tmp3, n)
-            a = sum( var(:) * fpn % deltaz(1:n) ) / fpn % totl /cmtoft ! W/ft
-            b = sum( fpn % deltaz(1:n) / fpn % dco(1:n) ) / fpn % totl / intoft ! 1/ft
-            fpn % qmpy(it) = a * b / pi * WtoBTUh ! BTUh/ft^2
-            fpn % qf(:) = tmp3(:) / sum(tmp3)
+            call linterp(var, driver % deltaz(1:n), tmp3, n)
+            a = sum( var(:) * driver % deltaz(1:n) ) / driver % totl /cmtoft ! W/ft
+            b = sum( driver % deltaz(1:n) / driver % dco(1:n) ) / driver % totl / intoft ! 1/ft
+            driver % qmpy(it) = a * b / pi * WtoBTUh ! BTUh/ft^2
+            driver % qf(:) = tmp3(:) / sum(tmp3)
         case("coolant temperature, C")
-            call linterp(var,  fpn % deltaz(1:n), tmp3, n)
-            fpn % tw(it) = tcf(tmp3(1))
-            fpn % coolanttemp(it,1:n+1) = (/( tcf(tmp3(i)), i = 1, n+1 )/)
+            call linterp(var,  driver % deltaz(1:n), tmp3, n)
+            driver % tw(it) = tcf(tmp3(1))
+            driver % coolanttemp(it,1:n+1) = (/( tcf(tmp3(i)), i = 1, n+1 )/)
         case("coolant pressure, MPa")
-            call linterp(var, fpn % deltaz(1:n), tmp3, n)
-            fpn % p2(it) = var(1) * MPatoPSI
-            fpn % coolantpressure(it,1:n+1) = tmp3(:) * MPatoPSI
+            call linterp(var, driver % deltaz(1:n), tmp3, n)
+            driver % p2(it) = var(1) * MPatoPSI
+            driver % coolantpressure(it,1:n+1) = tmp3(:) * MPatoPSI
         case("coolant mass flux, kg/(s*m^2)")
-            fpn % go(it) = var(1) * ksm2tolbhrft2
+            driver % go(it) = var(1) * ksm2tolbhrft2
         case default
             write(*,*) 'ERROR: Variable ', key, ' has not been found'
             stop
@@ -162,39 +162,39 @@ contains
                 do j = 1, m
                     ya = 1.d0
                     yb = 1.d0
-                    ra = fpn % hrad(j+1,i)
-                    rb = fpn % hrad(j,i)
-                    h = fpn % x(i+1) - fpn % x(i)
+                    ra = driver % hrad(j+1,i)
+                    rb = driver % hrad(j,i)
+                    h = driver % x(i+1) - driver % x(i)
                     volume = volume + linteg(ya,yb,ra,rb,h)
-                    ya = fpn % tmpfuel(j+1,i)
-                    yb = fpn % tmpfuel(j,i)
+                    ya = driver % tmpfuel(j+1,i)
+                    yb = driver % tmpfuel(j,i)
                     temper = temper + linteg(ya,yb,ra,rb,h)
                 enddo
                 var(i) = tfc(temper / volume)
             enddo
         case('bulk coolant temperature, C')
-            var(:) = 0.5d0 * ( fpn % BulkCoolantTemp(1:n) + fpn % BulkCoolantTemp(2:n+1) )
+            var(:) = 0.5d0 * ( driver % BulkCoolantTemp(1:n) + driver % BulkCoolantTemp(2:n+1) )
             var(:) = (/( tfc(var(i)), i = 1, n )/)
         case('gap conductance, W/(m^2*K)')
-            var(:) = fpn % GapCond(1:n) * Bhft2FtoWm2K
+            var(:) = driver % GapCond(1:n) * Bhft2FtoWm2K
         case('oxide thickness, um')
-            var(:) = fpn % EOSZrO2Thk(1:n) * 12000.d0 * miltoum
+            var(:) = driver % EOSZrO2Thk(1:n) * fttomil * miltoum
         case('thermal gap thickness, um')
-            var(:) = fpn % gapplot(1:n) * miltoum
+            var(:) = driver % gapplot(1:n) * miltoum
         case('mechanical gap thickness, um')
-            var(:) = fpn % FuelCladGap(1:n) * 1.D+3 * miltoum
+            var(:) = driver % FuelCladGap(1:n) * 1.D+3 * miltoum
         case('gap pressure, MPa')
-            var(:) = fpn % GapPress(1:n) * PSItoMPa
+            var(:) = driver % GapPress(1:n) * PSItoMPa
         case('cladding hoop strain, %')
-            var(:) = fpn % eps(1:n,1) * 100
+            var(:) = driver % eps(1:n,1) * 100
         case('cladding hoop stress, MPa')
-            var(:) = fpn % sig(1:n,1) * PSItoMPa
+            var(:) = driver % sig(1:n,1) * PSItoMPa
         case('cladding axial stress, MPa')
-            var(:) = fpn % sig(1:n,2) * PSItoMPa
+            var(:) = driver % sig(1:n,2) * PSItoMPa
         case('cladding radial stress, MPa')
-            var(:) = fpn % sig(1:n,3) * PSItoMPa
+            var(:) = driver % sig(1:n,3) * PSItoMPa
         case('axial mesh, cm')
-            var(:) = 0.5d0 * (fpn % x(1:n) + fpn % x(2:n+1)) / cmtoft
+            var(:) = 0.5d0 * (driver % x(1:n) + driver % x(2:n+1)) / cmtoft
         case default
             write(*,*) 'ERROR: Variable ', key, ' has not been found'
             stop
--- test/plot.py
+++ test/plot.py
@@ -1,8 +1,8 @@
 from numpy import *
 import matplotlib.pyplot as plt
 
-ref = loadtxt('ref2.out').T
-table = loadtxt('data2.out').T
+ref = loadtxt('../../frapcon_standalone/working/0.out').T
+table = loadtxt('0.out').T
 data, mesh = table[:-1], table[-1]
 
 names = [
@@ -17,13 +17,14 @@ names = [
 ]   
 
 for i, name in enumerate(names):
-    fig, ax = plt.subplots()
-    ax.plot(mesh, data[i], '-o', ms=10, lw=2, alpha=0.7, mfc='orange')
-    ax.plot(mesh, ref[i], '-o', ms=10, lw=2, alpha=0.7, mfc='red')
-    ax.set_xlabel('Axial elevation, cm')
-    ax.set_ylabel(name)
-    ax.legend(['NEW FRAPCON', 'OLD FRAPCON'])
-    ax.grid()
-    plt.savefig('../doc/figs/%s.png'%name.split(',')[0].replace(' ','_'))
+    if i == 3:
+        fig, ax = plt.subplots()
+        ax.plot(mesh, data[i], '-o', ms=10, lw=2, alpha=0.7, mfc='orange')
+        ax.plot(mesh, ref[i], '-o', ms=10, lw=2, alpha=0.7, mfc='red')
+        ax.set_xlabel('Axial elevation, cm')
+        ax.set_ylabel(name)
+        ax.legend(['NEW FRAPCON', 'OLD FRAPCON'])
+        ax.grid()
+    #plt.savefig('../doc/figs/%s.png'%name.split(',')[0].replace(' ','_'))
 
-#plt.show()
+plt.show()
