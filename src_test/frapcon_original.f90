3ac789de014c33c2215cccd879163d774d2ef289
alexlc <alex4444@list.ru>
2018-07-06 18:52:30 +0900

    Add original fraptran

--- src_test/frapcon_original.f90
+++ src_test/frapcon_original.f90
@@ -1,25 +1,4 @@
-PROGRAM FRAPCON_4_0_Patch_1
-    USE Kinds
-    USE Conversions
-    USE Variables, ONLY : ProblemTime, ounit, it
-    USE FileIO, ONLY : iofiles, IOEcho
-    USE TimeStep
-    USE, INTRINSIC :: ISO_FORTRAN_ENV
-    IMPLICIT NONE
-    !>@brief
-    !> This is the driver for the FRAPCON Code. Version 4.0 Patch 1.
-    !
-    REAL(r8k) :: LastTime
-    !
-    CALL iofiles
-    CALL IOEcho
-    !
-    CALL frpcon
-    !
-    CLOSE (ounit)
-    ! Print the time at which the code execution stopped (Convert from seconds to day)
-    LastTime = ProblemTime(it) * sectoday
-    WRITE (0,10) LastTime
-10  FORMAT ('FRAPCON code execution finished with a problem time of ',f11.4,' days.')
-    !
-END PROGRAM FRAPCON_4_0_Patch_1
+program main
+    use frapcon, only : FRAPCON_4_0_Patch_1
+    call FRAPCON_4_0_Patch_1
+end program main
---
+++ src_test/fraptran_original.f90
@@ -0,0 +1,4 @@
+program main
+    use fraptran, only : FRAPTRAN_1_5a
+    call FRAPTRAN_1_5a
+end program main
