711541b0742622d766bbf473467a20c432cfff4a
alexeycherezov <alex4444@list.ru>
2018-11-01 19:51:43 +0900

    Fixed time step output in the test input file

--- benchmark/REP-NA/run.py
+++ benchmark/REP-NA/run.py
@@ -100,12 +100,13 @@ names = [
 task = 'rep-na1'
 
 if True:
+    print "FRAPCON: "
     call(['../../build/debug/main_frapcon', 'rep-na1-frapcon.inp'])
     print "FRAPTRAN: "
     call(['../../build/debug/main_fraptran', 'rep-na1-fraptran.inp'])
-    print "FRAPI: "
-    call(['../../build/debug/test_frapi', "fraptran", '%s-fraptran.inp'%task, './restart-na1.txt', './%s-out.txt'%task])
     call(["../../utils/fraptran2h5.py", "%s-fraptran.plot"%task, "%s-fraptran.h5"%task])
-    call(["../../utils/frapi2h5.py", "%s-out.txt"%task, "%s-frapi.h5"%task])
+#    print "FRAPI: "
+#    call(['../../build/debug/test_frapi', "fraptran", '%s-fraptran.inp'%task, './restart-na1.txt', './%s-out.txt'%task])
+#    call(["../../utils/frapi2h5.py", "%s-out.txt"%task, "%s-frapi.h5"%task])
 
-draw(task)
+#draw(task)
